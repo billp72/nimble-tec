0222eb3eea3a80154714f93887f684448c34e5a2
billpope <epop.bill@gmail.com>
2019-10-03 09:56:52 -0400

    added more search features

--- deploy/public/js/javascript-debug.js
+++ deploy/public/js/javascript-debug.js
@@ -7,11 +7,13 @@ $(function($){
             $('.results').remove();
             $('#myDropdown').append('<a class="results">'+results.length+' results</a>');
             $('a').click(function(){
+                $('tbody').empty();
                 $.each(results, (index, row) => {
                     $('tbody').append($('<tr>').append(
-                        $('<td>').append('<input type="checkbox" />'),
+                        $('<td>').append('<input name="box-'+index+'" class="all" type="checkbox" />'),
                         $('<td>').append(row.firstName),
                         $('<td>').append(row.about),
+                        $('<td>').append(row.distance + ' mi'),
                         $('<td>').append(row.tel),
                         $('<td>').append(row.primary),
                         $('<td>').append(row.secondary),
@@ -19,12 +21,22 @@ $(function($){
                     ));
                 });
             });
+     
             if(results.length === 0){
                 $('.results').remove();
             }
+    
         });
     }
-    //
-    $('#search-bar').keyup($.debounce( 250, requested ) );
+ 
+    $('#search-bar').keyup($.debounce(250, requested));
 
+    $('#toggleCheckboxes').click(function(){
+        var checkboxes = Array.prototype.slice.call(document.getElementsByClassName('all'));
+        var len = checkboxes.length;
+        var index = 0;
+        for(index; index<len; index++){
+            checkboxes[index].checked = this.checked;
+        }
+    });
 }(jQuery));
